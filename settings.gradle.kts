414e42906a3e45ab6e84fa8ec0593a3796ef2acf
runner <runner@hmpps-github-actions-runner-856d8d7694-ww6jr>
2026-07-02 08:44:33 +0000

    Rename project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "hmpps-template-kotlin"
+rootProject.name = "hmpps-justice-data-agent-worker"
--- src/test/kotlin/uk/gov/justice/digital/hmpps/justicedataagentworker/integration/health/HealthCheckTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/justicedataagentworker/integration/health/HealthCheckTest.kt
@@ -1,7 +1,7 @@
-package uk.gov.justice.digital.hmpps.templatepackagename.integration.health
+package uk.gov.justice.digital.hmpps.justicedataagentworker.integration.health
 
 import org.junit.jupiter.api.Test
-import uk.gov.justice.digital.hmpps.templatepackagename.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.justicedataagentworker.integration.IntegrationTestBase
 
 class HealthCheckTest : IntegrationTestBase() {
 
