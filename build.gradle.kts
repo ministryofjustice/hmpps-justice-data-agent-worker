fd5d03fc324bde6614d5f47f0f99e5e6def7c3e0
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2026-09-17 10:49:56 +0100

    updating spring boot gradle plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.1")
-  // implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.4.1")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
   implementation("com.fasterxml.uuid:java-uuid-generator:5.2.0")
